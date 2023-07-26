d9b5041c52eae07561e66272129cb2873f38a28a
StevenDong2512 <stevendong1997@outlook.com>
2023-07-26 10:09:03 +1000

    changes

--- src/App.js
+++ src/App.js
@@ -21,6 +21,7 @@ function App() {
           <Route path="/portfolio" element={<Portfolio />} />
           <Route path="/contact" element={<Contact />} />
           <Route path="/resume" element={<Resume />} />
+          <Route path="*" element={<Home />} />
         </Routes>
       </div>
       <Footer />
